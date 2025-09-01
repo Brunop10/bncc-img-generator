4906dc2ddfefca37c95689fa2e08de1ff994fe9a
Brunop10 <brunoperussatto@gmail.com>
2025-09-01 14:56:51 -0300

    fix: sheets variables

--- src/services/googleSheetsService.js
+++ src/services/googleSheetsService.js
@@ -6,7 +6,12 @@ const config = require('../config/environment');
  */
 async function getGoogleSheetsClient() {
     const auth = new google.auth.GoogleAuth({
-        keyFile: config.GOOGLE_SHEETS.CREDENTIALS_PATH,
+        credentials: {
+            type: 'service_account',
+            project_id: process.env.GOOGLE_PROJECT_ID,
+            private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        },
         scopes: ['https://www.googleapis.com/auth/spreadsheets']
     });
     return google.sheets({ version: 'v4', auth });
